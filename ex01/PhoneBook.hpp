f29a9fad1ce0b25c2654214cb381b47777b3cf82
Hsametk <hsametk@gmail.com>
2025-07-07 14:48:00 +0300

    Non used libraries deleted.

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hakotu <hakotu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hakotu <hakotu@student.42istanbul.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 16:06:48 by hakotu            #+#    #+#             */
-/*   Updated: 2025/07/01 13:23:54 by hakotu           ###   ########.fr       */
+/*   Updated: 2025/07/07 14:25:19 by hakotu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 #include "Contact.hpp"
 #include <iomanip>
-#include <cstdlib>
 
 class PhoneBook
 {
